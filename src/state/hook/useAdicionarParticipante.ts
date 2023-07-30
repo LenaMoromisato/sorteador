f2968623babd56e162d9f6f6bd1e82c35abc2245
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-30 19:00:22 -0300

    Testes assíncronos

--- src/state/hook/useAdicionarParticipante.ts
+++ src/state/hook/useAdicionarParticipante.ts
@@ -8,6 +8,9 @@ export const useAdicionarParticipante = () => {
     return (nomeDoParticipante: string) => {
         if (lista.includes(nomeDoParticipante)) {
             setErro('Nomes duplicados não são permitidos!')
+            setTimeout(() => {
+                setErro("")
+            }, 5000)
             return
         }
         return setLista(listaAtual => [...listaAtual, nomeDoParticipante])
