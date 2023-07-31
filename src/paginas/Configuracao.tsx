90953d72c1b7d0cde663f9aef4f17bc8ee004ace
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-31 17:28:18 -0300

    Fotografando componentes

---
+++ src/paginas/Configuracao.tsx
@@ -0,0 +1,15 @@
+import Formulario from "../componentes/Formulario"
+import ListaParticipantes from "../componentes/ListaParticipantes"
+import Rodape from "../componentes/Rodape"
+
+const Configuracao = () => {
+    return (
+        <>
+            <Formulario />
+            <ListaParticipantes />
+            <Rodape />
+        </>
+    )
+}
+
+export default Configuracao
