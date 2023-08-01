c8d39eed2b04c412bf994bc159477073d6b007c1
LenaMoromisato <moromisato.lena@gmail.com>
2023-08-01 15:02:50 -0300

    Validando o sorteio

--- src/componentes/Rodape.test.tsx
+++ src/componentes/Rodape.test.tsx
@@ -11,10 +11,17 @@ jest.mock('../state/hook/useListaDeParticipantes', () => {
 })
 
 const mockNavegacao = jest.fn()
+const mockSorteio = jest.fn()
 
 jest.mock('react-router-dom', () => {
     return {
         useNavigate: () => mockNavegacao
+    }
+})
+
+jest.mock('../state/hook/useSorteador', () => {
+    return {
+        useSorteador: () => mockSorteio
     }
 })
 
@@ -66,5 +73,6 @@ describe('quando existem participantes suficientes', () => {
 
         expect(mockNavegacao).toHaveBeenCalledTimes(1)
         expect(mockNavegacao).toHaveBeenCalledWith('/sorteio')
+        expect(mockSorteio).toHaveBeenCalledTimes(1)
     })
 })
--- src/componentes/Rodape.tsx
+++ src/componentes/Rodape.tsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 import './Rodape.css'
+import { useSorteador } from "../state/hook/useSorteador"
 
 const Rodape = () => {
     const participantes = useListaDeParticipantes()
     const navegarPara = useNavigate()
+    const sortear = useSorteador()
     const iniciar = () => {
+        sortear()
         navegarPara('/sorteio')
     }
 
